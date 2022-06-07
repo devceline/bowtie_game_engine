fcdc30a0dd1cba2dc8c32943635cb5be7a3b8c3b
Celine Sarafa <celinesarafa@gmail.com>
2022-06-07 18:06:07 +0300

    Initialized Cargo and dependencies

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate glfw;
+
+use glfw::Context;
+
+fn main() {
+  let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+  let (mut window, events) = 
+    glfw.create_window(300, 300, "hi twitter", glfw::WindowMode::Windowed)
+    .expect("Failed to create glfw window");
+
+  window.make_current();
+  window.set_key_polling(true);
+
+  while !window.should_close() {
+    window.swap_buffers();
+
+    glfw.poll_events();
+
+    for(_, event) in glfw::flush_messages(&events) {
+      println!("{:?}", event);
+      match event {
+        glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) => {
+          window.set_should_close(true)
+        },
+        _ => {},
+      }
+    }
+  }
+}
