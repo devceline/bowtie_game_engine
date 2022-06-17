a0586626181651f3be65908ad9b9dadf6db48180
Celine Sarafa <celinesarafa@gmail.com>
2022-06-18 00:37:55 +0300

    Shaders now take in tex coords

--- shaders/fragment/main.glsl
+++ shaders/fragment/main.glsl
@@ -1,12 +1,15 @@
 #version 150 core
 
+uniform sampler2D tex;
 
 in vec3 Color;
+in vec2 tex_cords_out;
+
 out vec4 outColor;
-uniform vec3 triangleColor;
+
 
 void main()
 {
-		
-    outColor = vec4(Color, 1.0);
+    outColor = texture(tex, tex_cords_out) * vec4(Color, 1.0);
+    outColor = texture(tex, tex_cords_out);
 }
--- shaders/vertex/main.glsl
+++ shaders/vertex/main.glsl
@@ -2,11 +2,14 @@
 
 in vec2 position;
 in vec3 targetColor;
+in vec2 tex_cords_in;
 
 out vec3 Color;
+out vec2 tex_cords_out;
 
 void main()
 {
     Color = targetColor;
+		tex_cords_out = tex_cords_in;
     gl_Position = vec4(position, 0.0, 1.0);
 }
