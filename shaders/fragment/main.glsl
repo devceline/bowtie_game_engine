f6d2a0893619c315cba2be561791fa5540e9ba86
Celine Sarafa <celinesarafa@gmail.com>
2022-06-18 17:56:27 +0300

    Using colors in fragment shader to keep attribute

--- shaders/fragment/main.glsl
+++ shaders/fragment/main.glsl
@@ -10,7 +10,7 @@ out vec4 outColor;
 
 void main()
 {
-		vec4 tex1 = texture(patrick, tex_cords_out);
+		vec4 tex1 = texture(patrick, tex_cords_out) * vec4(Color, 1.0);
 		vec4 tex2 = texture(pride_flag, tex_cords_out);
     outColor = mix(tex1, tex2, 0.5);
 }
