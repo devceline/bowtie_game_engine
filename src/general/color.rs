ee8a95b95d0b6ceac0c2560a6c1b8c1dd7bcfaa7
Celine Sarafa <celinesarafa@gmail.com>
2022-06-18 17:55:28 +0300

    Color type abstraction and enum

---
+++ src/general/color.rs
@@ -0,0 +1,26 @@
+pub struct Color {
+  pub r: f32,
+  pub g: f32,
+  pub b: f32,
+  pub a: f32,
+}
+
+impl Color {
+  pub fn new(r: f32, g: f32, b: f32, a: f32) -> Color {
+    Color { r, g, b, a }
+  }
+}
+
+pub enum COLORS {
+  Red,
+  White
+}
+
+impl Into<Color> for COLORS {
+  fn into(self) -> Color {
+      match self {
+        COLORS::Red => Color::new(255.0, 0.0, 0.0, 1.0),
+        COLORS::White => Color::new(255.0, 255.0, 255.0, 1.0)
+      }
+  }
+}
