2329a823bc238ab4a62c4edfb0fb9e2f9ce31b16
Celine Sarafa <celinesarafa@gmail.com>
2022-06-19 23:29:24 +0300

    Drawer can now load sprites

--- src/rendering/drawer.rs
+++ src/rendering/drawer.rs
@@ -1,21 +1,28 @@
 use crate::general::color;
 use crate::gl_utils::element_array_buffer::ElementArrayBuffer;
 use crate::gl_utils::gl_translation::{DataType, DrawingMode, ToGl, UsageMode};
+use crate::gl_utils::shader_creator::ShaderProgram;
 use crate::gl_utils::vertex_array_buffer::VertexArrayBuffer;
 use crate::shapes::rectangle::Rectangle;
 use crate::shapes::shape::Shape;
+use crate::sprites::drawable::Drawable;
 
-pub struct Drawer {
+pub struct Drawer<'a> {
   vertex_array_buffer: VertexArrayBuffer<f32>,
   element_array_buffer: ElementArrayBuffer<i32>,
   vertices: Vec<f32>,
   elements: Vec<i32>,
   elements_count: i32,
-  dynamic_shapes: Vec<*const dyn Shape>,
+  dynamic_shapes: Vec<&'a dyn Shape>,
+  dynamic_sprites: Vec<&'a dyn Drawable<'a>>,
+  shader_program: &'a ShaderProgram,
 }
 
-impl Drawer {
-  pub fn new(usage_mode: UsageMode) -> Drawer {
+impl<'a> Drawer<'a> {
+  pub fn new(
+    usage_mode: UsageMode,
+    shader_program: &'a ShaderProgram 
+  ) -> Drawer<'a> {
     Drawer {
       vertex_array_buffer: VertexArrayBuffer::<f32>::new(
         DataType::Float32,
@@ -25,17 +32,19 @@ impl Drawer {
         DataType::UnsignedInt,
         usage_mode,
       ),
+      shader_program,
       vertices: vec![],
       elements: vec![],
       elements_count: 0,
       dynamic_shapes: vec![],
+      dynamic_sprites: vec![]
     }
   }
 
   fn load_shape(
     elements: &mut Vec<i32>,
     vertices: &mut Vec<f32>,
-    shape: *const dyn Shape,
+    shape: &'a dyn Shape,
     elements_count: i32,
   ) {
     unsafe {
@@ -49,7 +58,17 @@ impl Drawer {
     }
   }
 
-  pub fn load_shape_dynamic(&mut self, shape: *const dyn Shape) {
+  fn load_sprite(
+    elements: &mut Vec<i32>,
+    vertices: &mut Vec<f32>,
+    drawable: &'a dyn Drawable<'a>,
+    elements_count: i32,
+    ) -> () {
+      Drawer::load_shape(elements, vertices,  drawable.get_shape_ptr(), elements_count);
+  }
+
+
+  pub fn load_shape_dynamic(&mut self, shape: &'a dyn Shape) {
     self.dynamic_shapes.push(shape);
     Drawer::load_shape(
       &mut self.elements,
@@ -61,6 +80,21 @@ impl Drawer {
     self.element_array_buffer.update_data(&self.elements);
   }
 
+  pub fn load_sprite_dynamic(&mut self, sprite: &'a dyn Drawable<'a> )
+  {
+    self.dynamic_sprites.push(sprite);
+    Drawer::load_sprite(
+      &mut self.elements,
+      &mut self.vertices,
+      sprite,
+      self.elements_count,
+    );
+    self.vertex_array_buffer.update_data(&self.vertices);
+    self.element_array_buffer.update_data(&self.elements);
+
+    sprite.get_texture_ptr().load_texture(self.shader_program);
+  }
+
   pub fn clear_screen(&mut self, color: color::Color) {
     let clear_rect = Rectangle {
       x: -1.0,
@@ -70,8 +104,12 @@ impl Drawer {
       color,
     };
 
-    self.vertex_array_buffer.update_data(&clear_rect.get_vertices());
-    self.element_array_buffer.update_data(&clear_rect.get_elements());
+    self
+      .vertex_array_buffer
+      .update_data(&clear_rect.get_vertices());
+    self
+      .element_array_buffer
+      .update_data(&clear_rect.get_elements());
 
     unsafe {
       gl::DrawElements(
@@ -99,6 +137,10 @@ impl Drawer {
       self.vertex_array_buffer.update_data(&self.vertices);
       self.element_array_buffer.update_data(&self.elements);
     }
+
+    println!("Elements: {:?}", self.elements);
+    println!("Vertices: {:?}", self.vertices);
+    println!("Shapes: {:?}", self.dynamic_shapes);
 
     unsafe {
       gl::DrawElements(
