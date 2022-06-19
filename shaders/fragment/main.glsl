493a895c777b90f0170cf7ad17c44a7904d6d4a7
Celine Sarafa <celinesarafa@gmail.com>
2022-06-20 01:33:33 +0300

    Textures now take in TexId and multiple samplers

--- shaders/fragment/main.glsl
+++ shaders/fragment/main.glsl
@@ -1,16 +1,24 @@
 #version 150 core
 
-uniform sampler2D pride_flag;
-uniform sampler2D patrick;
+uniform sampler2D tex0_sampler;
+uniform sampler2D tex1_sampler;
 
 in vec4 Color;
 in vec2 tex_cords_out;
-
 out vec4 outColor;
+in float Tex_id;
 
 void main()
 {
-		vec4 tex1 = texture(patrick, tex_cords_out);
-		vec4 tex2 = texture(pride_flag, tex_cords_out);
-    outColor = mix(tex1, tex2, 0.5) * Color;
+	if (Tex_id == 0.0f) {
+		outColor = texture(tex0_sampler, tex_cords_out);
+	}
+	else if (Tex_id == 1.0f) {
+		outColor = texture(tex1_sampler, tex_cords_out);
+	}
+	else {
+		outColor = vec4(Color);
+	}
+
+
 }
--- shaders/vertex/main.glsl
+++ shaders/vertex/main.glsl
@@ -4,11 +4,15 @@ in vec2 position;
 in vec4 targetColor;
 in vec2 tex_cords_in;
 
+in float tex_id;
+
 out vec4 Color;
 out vec2 tex_cords_out;
+out float Tex_id;
 
 void main()
 {
+		Tex_id = tex_id;
     Color = targetColor;
 		tex_cords_out = tex_cords_in;
     gl_Position = vec4(position, 0.0, 1.0);
