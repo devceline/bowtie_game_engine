b4231c4082b255e839d94d77383e2954eabccf1c
Celine Sarafa <celinesarafa@gmail.com>
2022-06-22 01:14:56 +0300

    Matrix mul implementation
    - added UT for matrix x matrix

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,19 +1,17 @@
-use std::{fmt::Debug, ops::Mul};
+use std::ops::{Add, Index, Mul, Sub};
+
+pub trait Determinant<T> {
+  fn det(&self) -> T;
+}
 
 #[derive(Debug)]
-pub struct Matrix<T>
-where
-  T: Mul<T>,
-{
+pub struct Matrix<T> {
   _matrix: Vec<Vec<T>>,
   num_rows: usize,
   num_columns: usize,
 }
 
-impl<T> Matrix<T>
-where
-  T: Mul<T>,
-{
+impl<T> Matrix<T> {
   fn is_valid_matrix(matrix: &Vec<Vec<T>>) -> bool {
     for vec in matrix {
       if vec.len() != matrix[0].len() {
@@ -21,6 +19,18 @@ where
       }
     }
     return true;
+  }
+
+  pub fn get_num_columns(&self) -> usize {
+    self.num_columns
+  }
+
+  pub fn get_num_rows(&self) -> usize {
+    self.num_rows
+  }
+
+  pub fn len(&self) -> usize {
+    self.get_num_rows()
   }
 
   pub fn new(matrix: Vec<Vec<T>>) -> Matrix<T> {
@@ -36,6 +46,80 @@ where
       num_rows,
       num_columns,
     };
+  }
+}
+
+impl<T> Index<usize> for Matrix<T> {
+  type Output = Vec<T>;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self._matrix[index]
+  }
+}
+
+impl<T, TOther> Mul<Matrix<TOther>> for Matrix<T>
+where
+  TOther: Mul<T> + Into<T> + Copy,
+  T: Mul<T>
+    + Mul<TOther, Output = T>
+    + Sub<T, Output = T>
+    + Add<T, Output = T>
+    + Copy,
+{
+  type Output = Matrix<T>;
+  fn mul(self, rhs: Matrix<TOther>) -> Self::Output {
+    if self.get_num_columns() != rhs.get_num_rows() {
+      panic!("Not a valid matrix multiplication")
+    }
+
+    let mut new_matrix = Vec::<Vec<T>>::new();
+    new_matrix.reserve(self.get_num_rows());
+
+    for row_i in 0..self.get_num_rows() {
+      let mut row = Vec::<T>::new();
+
+      row.reserve(rhs.get_num_columns());
+      for col_i in 0..rhs.get_num_columns() {
+        let mut idx_sum = self[0][0] - self[0][0];
+        for rhs_row_i in 0..rhs.get_num_rows() {
+          idx_sum = idx_sum + (self[row_i][rhs_row_i] * rhs[rhs_row_i][col_i])
+        }
+        row.push(idx_sum)
+      }
+      new_matrix.push(row);
+    }
+
+    Matrix::new(new_matrix)
+  }
+}
+
+impl<T, TOther> Add<Matrix<TOther>> for Matrix<T>
+where
+  TOther: Add<T> + Into<T> + Copy,
+  T: Add<T> + Add<TOther, Output = T> + Copy,
+{
+  type Output = Matrix<T>;
+  fn add(self, rhs: Matrix<TOther>) -> Self::Output {
+    let mut new_matrix = Vec::<Vec<T>>::new();
+
+    if (self.get_num_columns() != rhs.get_num_columns())
+      || (self.get_num_rows() != rhs.get_num_rows())
+    {
+      panic!("Matrices don't have same number of columns or rows");
+    }
+
+    let num_columns = self.get_num_columns();
+
+    for vec_i in 0..self.len() {
+      let mut new_vec = Vec::<T>::new();
+      new_vec.reserve(num_columns);
+      for scalar_i in 0..self[vec_i].len() {
+        new_vec.push(self[vec_i][scalar_i] + rhs[vec_i][scalar_i]);
+      }
+      new_matrix.push(new_vec);
+    }
+
+    Matrix::new(new_matrix)
   }
 }
 
@@ -71,9 +155,9 @@ where
     if self.num_rows != other.num_rows {
       return false;
     }
-    for vec_i in 0..self._matrix.len() {
-      for scalar_i in 0..self._matrix[vec_i].len() {
-        if self._matrix[vec_i][scalar_i] != other._matrix[vec_i][scalar_i] {
+    for vec_i in 0..self.len() {
+      for scalar_i in 0..self[vec_i].len() {
+        if self[vec_i][scalar_i] != other[vec_i][scalar_i] {
           return false;
         }
       }
@@ -82,5 +166,54 @@ where
   }
   fn ne(&self, other: &Self) -> bool {
     !self.eq(other)
+  }
+}
+
+impl<T> Determinant<T> for Matrix<T>
+where
+  T: Mul<T, Output = T> + Add<T, Output = T> + Sub<T, Output = T> + Copy,
+{
+  /// Calculates determinant using Laplace expansion
+  fn det(&self) -> T {
+    if self.num_columns != self.num_rows {
+      panic!("Matrix is not a square");
+    }
+
+    if self.num_columns == 2 && self.num_rows == 2 {
+      return (self[0][0] * self[1][1]) - (self[1][0] * self[0][1]);
+    }
+
+    let mut determinant = self[0][0] - self[0][0];
+
+    for col_i in 0..self.get_num_columns() {
+      let scalar = self[0][col_i];
+
+      let mut smaller_mat = Vec::<Vec<T>>::new();
+
+      // Skipping the row that includes the scalar
+      for row_i in 1..self.get_num_rows() {
+        let mut new_row = Vec::<T>::new();
+
+        for item_i in 0..self[row_i].len() {
+          if item_i == col_i {
+            continue;
+          }
+          new_row.push(self[row_i][item_i]);
+        }
+
+        smaller_mat.push(new_row);
+      }
+
+      let smaller_mat_mat = Matrix::new(smaller_mat);
+
+      let block_determinant = smaller_mat_mat.det() * scalar;
+
+      if col_i == 0 || col_i % 2 == 0 {
+        determinant = determinant + block_determinant;
+      } else {
+        determinant = determinant - block_determinant;
+      }
+    }
+    determinant
   }
 }
--- src/math/test.rs
+++ src/math/test.rs
@@ -1,5 +1,4 @@
 #[cfg(test)]
-
 mod tests {
   #[test]
   fn scalar_matrix_multiply_ok() {
@@ -23,5 +22,126 @@ mod tests {
     use crate::math::matrix::Matrix;
     // Unequal columns
     Matrix::new(vec![vec![1, 2], vec![1, 2, 3]]);
+  }
+
+  #[test]
+  fn matrix_matrix_add_ok() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![1, -7, 5], vec![0, 3, -10]]);
+    let rhs_matrix = Matrix::new(vec![vec![5, 0, 3], vec![11, -1, 7]]);
+    let test_matrix = Matrix::new(vec![vec![6, -7, 8], vec![11, 2, -3]]);
+    assert_eq!(matrix + rhs_matrix, test_matrix);
+  }
+
+  #[test]
+  fn matrix_matrix_add_bad() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![1, -7, 5], vec![0, 3, -10]]);
+    let rhs_matrix = Matrix::new(vec![vec![5, 0, 3], vec![11, -1, 7]]);
+    let test_matrix = Matrix::new(vec![vec![6, 0, 0], vec![11, 2, -3]]);
+    assert_ne!(matrix + rhs_matrix, test_matrix);
+  }
+
+  #[test]
+  #[should_panic(
+    expected = "Matrices don't have same number of columns or rows"
+  )]
+  fn matrix_matrix_add_validation() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![1, -7, 5], vec![0, 3, -10]]);
+    let rhs_matrix = Matrix::new(vec![vec![5, 0], vec![11, -1]]);
+    let _ = matrix + rhs_matrix;
+  }
+
+  #[test]
+  fn matrix_2x2_determinant() {
+    use crate::math::matrix::{Determinant, Matrix};
+    let matrix = Matrix::new(vec![vec![1, 2], vec![3, 4]]);
+    let test_det = -2;
+    assert_eq!(matrix.det(), test_det);
+  }
+
+  #[test]
+  fn matrix_3x3_determinant() {
+    use crate::math::matrix::{Determinant, Matrix};
+    let matrix = Matrix::new(vec![vec![6, 1, 1], vec![4, -2, 5], vec![2, 8, 7]]);
+    let test_det = -306;
+    assert_eq!(matrix.det(), test_det);
+  }
+
+  #[test]
+  fn matrix_4x4_determinant() {
+    use crate::math::matrix::{Determinant, Matrix};
+    let matrix = Matrix::new(vec![
+      vec![6, 1, 1, 3],
+      vec![4, -2, 5, 10],
+      vec![2, 8, 7, -8],
+      vec![7, 9, 10, -9],
+    ]);
+    let test_det = 1464;
+    assert_eq!(matrix.det(), test_det);
+  }
+
+  #[test]
+  fn matrix_5x5_determinant() {
+    use crate::math::matrix::{Determinant, Matrix};
+    let matrix = Matrix::new(vec![
+      vec![5,6,19,10,0],
+      vec![12,17,8,18,9],
+      vec![10,10,22,2,3],
+      vec![67,8,9,0,0],
+      vec![-3, 45, 3, 2, 1],
+    ]);
+    let test_det = 6709072;
+    assert_eq!(matrix.det(), test_det);
+  }
+
+  #[test]
+  #[should_panic(expected = "Matrix is not a square")]
+  fn matrix_determinant_validates() {
+    use crate::math::matrix::{Determinant, Matrix};
+    let matrix = Matrix::new(vec![
+      vec![6, 1, 1],
+      vec![4, -2, 5],
+      vec![2, 8, 7],
+      vec![7, 9, 10],
+    ]);
+    let test_det = 1464;
+    assert_eq!(matrix.det(), test_det);
+  }
+
+  #[test]
+  fn matrix_2x3_dot_3x2_ok() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![0, 3, 5], vec![5, 5, 2]]);
+    let rhs = Matrix::new(vec![vec![3, 4], vec![3, -2], vec![4, -2]]);
+    let test_mat = Matrix::new(vec![vec![29, -16], vec![38, 6]]);
+    assert_eq!(matrix * rhs, test_mat);
+  }
+
+  #[test]
+  fn matrix_3x3_dot_3x3_ok() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![12, 7, 8], vec![99, -1, 8], vec![7, 6, -1]]);
+    let rhs = Matrix::new(vec![vec![28, 9, 0 ], vec![0, 12, 3], vec![-9, 8 ,10]]);
+    let test_mat = Matrix::new(vec![vec![264, 256, 101], vec![2700, 943, 77], vec![205, 127, 8]]);
+    assert_eq!(matrix * rhs, test_mat);
+  }
+
+  fn matrix_2x1_dot_1x2_ok() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![12, 7, 8], vec![99, -1, 8], vec![7, 6, -1]]);
+    let rhs = Matrix::new(vec![vec![28, 9, 0 ], vec![0, 12, 3], vec![-9, 8 ,10]]);
+    let test_mat = Matrix::new(vec![vec![264, 256, 101], vec![2700, 943, 77], vec![205, 127, 8]]);
+    assert_eq!(matrix * rhs, test_mat);
+  }
+
+  #[test]
+  #[should_panic(expected = "Not a valid matrix multiplication")]
+  fn matrix_dot_validates() {
+    use crate::math::matrix::Matrix;
+    let matrix = Matrix::new(vec![vec![0, 3, 5], vec![5, 5, 2]]);
+    let rhs = Matrix::new(vec![vec![3, 4], vec![3, -2]]);
+    let _ = matrix * rhs;
   }
 }
