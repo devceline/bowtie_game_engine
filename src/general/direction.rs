8c70acdb99144dffd51bc56d74b42aaef35b351b
Celine Sarafa <celinesarafa@gmail.com>
2022-06-30 12:56:54 +0300

    Direction updated to be more accurate
    - Also added utility to transform them to and from floats

--- src/general/direction.rs
+++ src/general/direction.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Direction {
   Up,
   Right,
@@ -11,44 +11,105 @@ pub enum Direction {
   Stationary,
 }
 
+impl From<i32> for Direction {
+  fn from(num: i32) -> Self {
+    match num {
+      0 => Direction::Up,
+      1 => Direction::Right,
+      2 => Direction::Down,
+      3 => Direction::Left,
+      4 => Direction::UpRight,
+      5 => Direction::DownRight,
+      6 => Direction::UpLeft,
+      7 => Direction::DownLeft,
+      8 => Direction::Stationary,
+      _ => {
+        panic!("Not a direction float {num}");
+      }
+    }
+  }
+}
+
+impl Into<i32> for Direction {
+  fn into(self) -> i32 {
+    match self {
+      Direction::Up => 0,
+      Direction::Right => 1,
+      Direction::Down => 2,
+      Direction::Left => 3,
+      Direction::UpRight => 4,
+      Direction::DownRight => 5,
+      Direction::UpLeft => 6,
+      Direction::DownLeft => 7,
+      Direction::Stationary => 8,
+    }
+  }
+}
+
+impl From<f32> for Direction {
+  fn from(num: f32) -> Self {
+    let num_int = num as i32;
+    return Direction::from(num_int);
+  }
+}
+
+impl Into<f32> for Direction {
+  fn into(self) -> f32 {
+    let num_int: i32 = self.into();
+    return num_int as f32;
+  }
+}
+
 impl Direction {
   pub fn subtract_direction(&self, other: Direction) -> Direction {
     let res = match self {
       Direction::Stationary => Direction::Stationary,
       Direction::Right => match other {
         Direction::Right => Direction::Stationary,
+        Direction::UpRight => Direction::Stationary,
+        Direction::DownRight => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::Left => match other {
         Direction::Left => Direction::Stationary,
+        Direction::UpLeft => Direction::Stationary,
+        Direction::DownLeft => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::Up => match other {
         Direction::Up => Direction::Stationary,
+        Direction::UpRight => Direction::Stationary,
+        Direction::UpLeft => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::Down => match other {
         Direction::Down => Direction::Stationary,
+        Direction::DownLeft => Direction::Stationary,
+        Direction::DownRight => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::UpLeft => match other {
         Direction::Up => Direction::Left,
         Direction::Left => Direction::Up,
+        Direction::UpLeft => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::UpRight => match other {
         Direction::Right => Direction::Up,
         Direction::Up => Direction::Right,
+        Direction::UpRight => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::DownRight => match other {
         Direction::Down => Direction::Right,
         Direction::Right => Direction::Down,
+        Direction::DownRight => Direction::Stationary,
         _ => self.to_owned(),
       },
       Direction::DownLeft => match other {
         Direction::Down => Direction::Left,
         Direction::Left => Direction::Down,
+        Direction::DownLeft => Direction::Stationary,
         _ => self.to_owned(),
       },
     };
@@ -83,23 +144,23 @@ impl Direction {
         _ => self.to_owned(),
       },
       Direction::UpLeft => match other {
-        Direction::Right => Direction::Stationary,
-        Direction::Down => Direction::Stationary,
+        Direction::Right => Direction::Up,
+        Direction::Down => Direction::Left,
         _ => self.to_owned(),
       },
       Direction::UpRight => match other {
-        Direction::Left => Direction::Stationary,
-        Direction::Down => Direction::Stationary,
+        Direction::Left => Direction::Up,
+        Direction::Down => Direction::Right,
         _ => self.to_owned(),
       },
       Direction::DownRight => match other {
         Direction::Left => Direction::Stationary,
-        Direction::Up => Direction::Stationary,
+        Direction::Up => Direction::Right,
         _ => self.to_owned(),
       },
       Direction::DownLeft => match other {
         Direction::Right => Direction::Stationary,
-        Direction::Up => Direction::Stationary,
+        Direction::Up => Direction::Left,
         _ => self.to_owned(),
       },
     };
