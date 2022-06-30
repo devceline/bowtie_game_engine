79f8aea30981446392f7e1fdd2267eb2fc7cc7a6
Celine Sarafa <celinesarafa@gmail.com>
2022-06-30 13:18:04 +0300

    Playing around with new entity system

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate gl;
 extern crate glfw;
 extern crate png;
 
+mod components;
 mod game_objects;
 mod general;
 mod gl_utils;
@@ -11,25 +12,27 @@ mod math;
 mod rendering;
 mod shapes;
 mod sprites;
-mod components;
 
 use components::collide::CollisionComponent;
+use components::event::EventComponent;
+use components::gravity::GravityComponent;
+use game_objects::floor::Floor;
 use glfw::Context;
 
+use game_objects::playable_character::PlayableCharacter;
 use general::color::COLORS;
 use gl_utils::gl_error_reader;
 use gl_utils::gl_texture::{Texture, TextureOptions};
 use god_object::entity::Entity;
-use game_objects::playable_character::PlayableCharacter;
 use god_object::god_object::BowTie;
 use shapes::rectangle::Rectangle;
 use sprites::sprite::Sprite;
 
-
-async fn handle_events<'a>(
+async fn handle_player_events<'a>(
   event: glfw::WindowEvent,
-  character: &mut Sprite<'a, Rectangle>,
+  character: &mut PlayableCharacter<'a>,
 ) {
+  // println!("x: {}, y: {}, y+height: {}", character.get_x(), character.get_y(), character.get_y() + character.get_height());
   futures::join!(character.respond_to_event(&event));
 }
 
@@ -54,70 +57,67 @@ fn window_setup(glfw: &mut glfw::Glfw, window: &mut glfw::Window) {
 fn main() {
   let mut glfw_instance = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
   let (mut window, events) = glfw_instance
-    .create_window(1031, 540, "rust game engine", glfw::WindowMode::Windowed)
+    .create_window(1000, 800, "rust game engine", glfw::WindowMode::Windowed)
     .expect("Failed to create window");
   window_setup(&mut glfw_instance, &mut window);
 
   gl_error_reader::init_debug_callback();
-  let mut god_object = BowTie::new();
+  let mut bowtie = BowTie::new();
 
-
-  // let sky = Sprite::new(
-  //   Rectangle::new(-1.0, 1.0, 2.0, 2.0, COLORS::White.into()),
-  //   Texture::new("sky", TextureOptions::default()),
-  // );
-  // let floor = Sprite::new(
-  //   Rectangle::new(-1.0, -0.5, 2.0, 0.5, COLORS::White.into()),
-  //   Texture::new("floor", TextureOptions::default()),
-  // );
-  // let mut game_world = GameWorld::new(floor, sky);
-
-
-  let mut playable_character = PlayableCharacter::new(Sprite::new(Rectangle::new(
-      -0.5, -0.5, 0.3, 0.2, COLORS::White.into()
-      ), Texture::new("character", TextureOptions::default())));
   let mut collision_component = CollisionComponent::new();
+  let mut gravity_component = GravityComponent::new(0.01);
+  let mut event_component = EventComponent::new();
 
-  let mut playable_character2 = PlayableCharacter::new(Sprite::new(Rectangle::new(
-      0.5, -0.5, 0.3, 0.2, COLORS::White.into()
-      ), Texture::new("character", TextureOptions::default())));
+  let mut floor = Floor::new();
+
+  let mut playable_character = PlayableCharacter::new(Sprite::new(
+    Rectangle::new(0.0, 0.5, 0.2, 0.3, COLORS::White.into()),
+    Texture::new("witch", TextureOptions::default()),
+  ));
+
+  let mut random_entities = Vec::<PlayableCharacter>::new();
 
   playable_character.load_components(&mut collision_component);
-  // playable_character2.load_components(&mut collision_component);
+  playable_character.load_components(&mut gravity_component);
+  playable_character.load_components(&mut event_component);
+  floor.load_components(&mut collision_component);
+  bowtie.load_entity(&mut floor);
+  bowtie.load_entity(&mut playable_character);
 
-  god_object.load_entity(&mut playable_character);
-  god_object.load_entity(&mut playable_character2);
-
-  god_object.prep_for_render();
+  bowtie.prep_for_render();
 
   while !window.should_close() {
     window.swap_buffers();
     glfw_instance.poll_events();
 
-    god_object.update_entities();
-    let is_collided = collision_component.get_is_collided(&mut playable_character);
+    let collision_direction = collision_component
+      .get_entity_collision_direction(&mut playable_character);
+    bowtie.update_entities();
+    let is_collided =
+      collision_component.get_is_collided(&mut playable_character);
     if is_collided {
-      playable_character.set_color_overlay(COLORS::Red.into());
+      playable_character.set_collision_direction(collision_direction);
+    } else {
+      playable_character.set_collision_direction(collision_direction);
     }
-    else {
-      playable_character.set_color_overlay(COLORS::White.into());
-    }
-    god_object.draw_entities();
+    bowtie.draw_entities();
 
     for (_, event) in glfw::flush_messages(&events) {
-      // futures::executor::block_on(handle_events(
-      //   event.to_owned(),
-      //   &mut character,
-      // ));
+      futures::executor::block_on(handle_player_events(
+        event.to_owned(),
+        &mut playable_character,
+      ));
       match event {
         glfw::WindowEvent::Key(glfw::Key::Escape, _, _, _) => {
           window.set_should_close(true);
         }
-        glfw::WindowEvent::Key(glfw::Key::Left, _, _, _) => {
-          playable_character.move_left();
-        }
-        glfw::WindowEvent::Key(glfw::Key::Right, _, _, _) => {
-          playable_character.move_right();
+        glfw::WindowEvent::Key(glfw::Key::O, _, _, _) => {
+          random_entities.push(PlayableCharacter::new(Sprite::new(
+            Rectangle::new(0.0, 0.5, 0.2, 0.3, COLORS::White.into()),
+            Texture::new("character", TextureOptions::default()),
+          )));
+          let id = random_entities.len() - 1;
+          bowtie.load_entity(&mut random_entities[id]);
         }
         _ => {}
       }
